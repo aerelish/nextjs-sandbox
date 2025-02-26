e9488351078297c31c0b65da5ff0afe2cfe906b8
aerelish <elisharagas@gmail.com>
2025-02-27 01:24:45 +0800

    removing usecontext feature for now

--- src/app/react-hook-forms/page.tsx
+++ src/app/react-hook-forms/page.tsx
@@ -1,35 +1,10 @@
-'use client'
-
-import { useState } from "react";
-
-import { useFormContext } from "@/app/react-hook-forms/_components/form-context";
-
 import SimpleForm from "@/app/react-hook-forms/_components/simple-form-no-rhf";
-import SimpleFormRHF from "@/app/react-hook-forms/_components/simple-form-no-rhf";
 
 export default function Page() {
-
-  const [page, setPage] = useState(<SimpleForm />);
-  const { form } = useFormContext();
-
-  const renderSwitch = (form: 'no-rhf' | 'using-rhf') => {
-
-    switch (form) {
-      case "no-rhf":
-        setPage(<SimpleForm />);
-      case "using-rhf":
-        setPage(<SimpleFormRHF />);
-      default:
-        setPage(<SimpleForm />);;
-    }
-    return page;
-
-  };
-
   return (
     <div className="min-w-full flex justify-center p-8">
       <div className="p-12 border border--border rounded-lg">
-        {renderSwitch(form)}
+        <SimpleForm />
       </div>
     </div>
   );
